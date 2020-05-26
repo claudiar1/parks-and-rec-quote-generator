84ba2d1055449ef804c78d0c1a8dbaf3645947d3
Claudia Robinson <claudiamegr@gmail.com>
2020-05-26 12:37:41 -0400

    Updated property names per Treehouse grading expectations and doubled lenght of timer for improved UX.

--- js/script.js
+++ js/script.js
@@ -2,56 +2,56 @@ let quotes = [
   {
     quote: `If you ever speak to me in Spanish, please use the formal 'usted.'`,
     source: `April Ludgate`,
-    epName: `"Jerry's Painting"`,
-    epNum: `Season 3, Episode 11`
+    citation: `"Jerry's Painting"`,
+    year: `Season 3, Episode 11`
   },
   {
     quote: `Just remember, every time you look up at the moon, I, too, will be looking at a moon. Not the same moon, obviously. That's impossible.`,
     source: `Andy Dwyer`,
-    epName: `"Filibuster"`,
-    epNum: `Season 6, Episode 5`
+    citation: `"Filibuster"`,
+    year: `Season 6, Episode 5`
   },
   {
     quote: `Sometimes you gotta work a little so you can ball a lot.`,
     source: `Tom Haverford`,
-    epName: `"Ben's Parents"`,
-    epNum: `Season 5, Episode 6`
+    citation: `"Ben's Parents"`,
+    year: `Season 5, Episode 6`
   },
   {
     quote: `I'm going to tell you all of my secrets: I once forgot to brush my teeth for five weeks. I didn't actually sell my car, I just forgot where I parked it. I don't know who Al Gore is and at this point I'm too afraid to ask. When they say 2% milk I don't know what the other 98% is. When I was a baby my head was so big scientists did experiments on me. I once threw a beer at a swan and then it attacked my niece, Rebecca.`,
     source: `Andy Dwyer`,
-    epName: `"New Slogan"`,
-    epNum: `Season 6, Episode 15`
+    citation: `"New Slogan"`,
+    year: `Season 6, Episode 15`
   },
   {
     quote: `No matter what I do, literally nothing bad can happen to me. I'm like a white male U.S. Senator.`,
     source: `Leslie Knope`,
-    epName: `"Fluoride"`,
-    epNum: `Season 6, Episode 7`
+    citation: `"Fluoride"`,
+    year: `Season 6, Episode 7`
   },
   {
     quote: `No soup in the Benz.`,
     source: `Donna Meagle`,
-    epName: `"Pawnee Rangers"`,
-    epNum: `Season 4, Episode 4`
+    citation: `"Pawnee Rangers"`,
+    year: `Season 4, Episode 4`
   },
   {
     quote: `It's like I always say. When life give you lemons, you sell some of your grandma's jewelry and go clubbing.`,
     source: `Jean-Ralphio`,
-    epName: `"London"`,
-    epNum: `Season 6, Episode 1`
+    citation: `"London"`,
+    year: `Season 6, Episode 1`
   },
   {
     quote: `I really only listen to, like, German Death Reggae and Halloween sound effects records from the 1950s. And Bette Midler. Obviously.`,
     source: `April Ludgate`,
-    epName: `"Swing Vote"`,
-    epNum: `Season 5, Episode 21`
+    citation: `"Swing Vote"`,
+    year: `Season 5, Episode 21`
   },
   {
     quote: `I'm allergic to sushi. Every time I eat more than 80 sushis, I barf.`,
     source: `Andy Dwyer`,
-    epName: `"Ron and Diane"`,
-    epNum: `Season 5, Episode 9`
+    citation: `"Ron and Diane"`,
+    year: `Season 5, Episode 9`
   },
   {
     quote: `Treat yo'self!`,
@@ -142,11 +142,11 @@ function getRandomColor() {
 function printQuote() {
   let randomQuote = getRandomQuote();
   let myString = `<p class="quote">${randomQuote.quote}</p> <p class="source">${randomQuote.source}`;
-  if (randomQuote.epName) {
-    myString += `<span class="citation">${randomQuote.epName}</span>`;
+  if (randomQuote.citation) {
+    myString += `<span class="citation">${randomQuote.citation}</span>`;
   }
-  if (randomQuote.epNum) {
-    myString += `<span class="year">${randomQuote.epNum}</span>`;
+  if (randomQuote.year) {
+    myString += `<span class="year">${randomQuote.year}</span>`;
   }
   if (randomQuote.tag) {
     myString += `, ${randomQuote.tag}`;
@@ -161,7 +161,7 @@ function printQuote() {
  *
  * Auto refreshes the page and renders a new random quote every 10 seconds.
  */
-setInterval(printQuote, 10000);
+setInterval(printQuote, 20000);
 
 document
   .getElementById("load-quote")
