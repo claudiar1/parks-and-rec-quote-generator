af5d9c98de3787b2e4084c0710e8395deec10730
Claudia Robinson <claudiamegr@gmail.com>
2020-08-21 13:48:17 -0400

    Add index.md file

---
+++ index.md
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Random Quotes</title>
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+
+  <body>
+    <header>
+      <h1>Random Parks & Rec Quotes</h1>
+      <button id="load-quote" class="load-quote">Show another quote</button>
+    </header>
+
+    <div class="container">
+      <div id="quote-box" class="quote-box">
+        <p class="quote">
+          Click the <em>Show another quote button</em> above to get a randomly
+          generated Parks & Rec quote.
+        </p>
+        <p class="source">
+          Claudia Robinson<span class="citation">Developer of this project</span
+          ><span class="year">2020</span>
+        </p>
+      </div>
+    </div>
+
+    <script src="js/script.js"></script>
+
+  </body>
+</html>
--- js/script.js
+++ js/script.js
@@ -3,107 +3,107 @@ let quotes = [
     quote: `If you ever speak to me in Spanish, please use the formal 'usted.'`,
     source: `April Ludgate`,
     citation: `"Jerry's Painting"`,
-    year: `Season 3, Episode 11`
+    year: `Season 3, Episode 11`,
   },
   {
     quote: `Just remember, every time you look up at the moon, I, too, will be looking at a moon. Not the same moon, obviously. That's impossible.`,
     source: `Andy Dwyer`,
     citation: `"Filibuster"`,
-    year: `Season 6, Episode 5`
+    year: `Season 6, Episode 5`,
   },
   {
     quote: `Sometimes you gotta work a little so you can ball a lot.`,
     source: `Tom Haverford`,
     citation: `"Ben's Parents"`,
-    year: `Season 5, Episode 6`
+    year: `Season 5, Episode 6`,
   },
   {
     quote: `I'm going to tell you all of my secrets: I once forgot to brush my teeth for five weeks. I didn't actually sell my car, I just forgot where I parked it. I don't know who Al Gore is and at this point I'm too afraid to ask. When they say 2% milk I don't know what the other 98% is. When I was a baby my head was so big scientists did experiments on me. I once threw a beer at a swan and then it attacked my niece, Rebecca.`,
     source: `Andy Dwyer`,
     citation: `"New Slogan"`,
-    year: `Season 6, Episode 15`
+    year: `Season 6, Episode 15`,
   },
   {
     quote: `No matter what I do, literally nothing bad can happen to me. I'm like a white male U.S. Senator.`,
     source: `Leslie Knope`,
     citation: `"Fluoride"`,
-    year: `Season 6, Episode 7`
+    year: `Season 6, Episode 7`,
   },
   {
     quote: `No soup in the Benz.`,
     source: `Donna Meagle`,
     citation: `"Pawnee Rangers"`,
-    year: `Season 4, Episode 4`
+    year: `Season 4, Episode 4`,
   },
   {
     quote: `It's like I always say. When life give you lemons, you sell some of your grandma's jewelry and go clubbing.`,
     source: `Jean-Ralphio`,
     citation: `"London"`,
-    year: `Season 6, Episode 1`
+    year: `Season 6, Episode 1`,
   },
   {
     quote: `I really only listen to, like, German Death Reggae and Halloween sound effects records from the 1950s. And Bette Midler. Obviously.`,
     source: `April Ludgate`,
     citation: `"Swing Vote"`,
-    year: `Season 5, Episode 21`
+    year: `Season 5, Episode 21`,
   },
   {
     quote: `I'm allergic to sushi. Every time I eat more than 80 sushis, I barf.`,
     source: `Andy Dwyer`,
     citation: `"Ron and Diane"`,
-    year: `Season 5, Episode 9`
+    year: `Season 5, Episode 9`,
   },
   {
     quote: `Treat yo'self!`,
     source: `Tom Haverford and Donna Meagle`,
-    tag: `Classic Quote`
+    tag: `Classic Quote`,
   },
   {
     quote: `I have no idea what I'm doing, but I know I'm doing it really, really well.`,
-    source: `Andy Dwyer`
+    source: `Andy Dwyer`,
   },
   {
     quote: `Just give me all the bacon and eggs you have. Wait, wait. I worry what you just heard was, "give me a lot of bacon and eggs." What I said was, "give me all the bacon and eggs you have." Do you understand?`,
-    source: `Ron Swanson`
+    source: `Ron Swanson`,
   },
   {
     quote: `I made my money the old fasioned way - I got run over by a Lexus.`,
-    source: `Jean-Ralphio`
+    source: `Jean-Ralphio`,
   },
   {
     quote: `One time my refrigerator stopped working. I didn't know what to do. I just moved.`,
-    source: `Tom Haverford`
+    source: `Tom Haverford`,
   },
   {
     quote: `Time is money, money is power, power is pizza, pizza is knowledge. Let's go!`,
-    source: `April Ludgate`
+    source: `April Ludgate`,
   },
   {
     quote: `I'll have a glass of your most expensive red wine mixed with a glass of your cheapest white wine served in a dog bowl. Silly straws all around, please.`,
-    source: `April Ludgate`
+    source: `April Ludgate`,
   },
   {
     quote: `I tried to make ramen in the coffee pot and I broke everything.`,
     source: `Andy Dwyer`,
     epName: `"Comeback Kid"`,
-    epNum: `Season 4`
+    epNum: `Season 4`,
   },
   {
     quote: `April is the best. But she's 20. When April was born, I was already in the third grade, which means if we were friends back then, I would have been hanging out with a baby. I don't know anything about infant care. My god, I could have killed her.`,
-    source: `Andy Dwyer`
+    source: `Andy Dwyer`,
   },
   {
     quote: `When I get bummed out, I take my shirt off because the bad feelings make me feel sweaty.`,
-    source: `Andy Dwyer`
+    source: `Andy Dwyer`,
   },
   {
     quote: `Did you know that the food you eat becomes energy? Boom! That's spaghetti. Nachos. That's a cookie.`,
-    source: `Andy Dwyer`
+    source: `Andy Dwyer`,
   },
   {
     quote: `I once ate a Twix with the wrapper on it and I've never seen the wrapper come out.`,
-    source: `Andy Dwyer`
-  }
+    source: `Andy Dwyer`,
+  },
 ];
 
 let previousNumber;
